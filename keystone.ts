8e2903a03eaae90162d9ce677bfc422e421f151e
devsadikbd <devsadikbd@gmail.com>
2026-03-18 20:14:01 +0600

    first commit

--- keystone.ts
+++ keystone.ts
@@ -1,34 +1,34 @@
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
 import {
   withItemData,
   statelessSessions,
-} from '@keystone-next/keystone/session';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
-import { ProductImage } from './schemas/ProductImage';
-import 'dotenv/config';
-import { insertSeedData } from './seed-data';
-import { sendPasswordResetEmail } from './lib/mail';
-import { CartItem } from './schemas/CartItem';
-import { extendGraphqlSchema } from './mutations/index';
-import { Order } from './schemas/Order';
-import { OrderItem } from './schemas/OrderItem';
-import { Role } from './schemas/Role';
-import { permissionsList } from './schemas/fields';
+} from "@keystone-next/keystone/session";
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
+import "dotenv/config";
+import { insertSeedData } from "./seed-data";
+import { sendPasswordResetEmail } from "./lib/mail";
+import { CartItem } from "./schemas/CartItem";
+import { extendGraphqlSchema } from "./mutations/index";
+import { Order } from "./schemas/Order";
+import { OrderItem } from "./schemas/OrderItem";
+import { Role } from "./schemas/Role";
+import { permissionsList } from "./schemas/fields";
 
 const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost:27017/sadik';
+  process.env.DATABASE_URL || "mongodb://localhost:27017/sadik";
 const sessionConfig = {
   MaxAge: 60 * 60 * 24 * 365,
-  secret: process.env.COOKIE_SECRET || 'a-very-secure-secret',
+  secret: process.env.COOKIE_SECRET || "a-very-secure-secret",
 };
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
   },
   passwordResetLink: {
     async sendToken(args) {
@@ -45,10 +45,10 @@ export default withAuth(
       },
     },
     db: {
-      adapter: 'mongoose',
+      adapter: "mongoose",
       url: databaseURL,
       onConnect: async (keystone) => {
-        if (process.argv.includes('--seed-data')) {
+        if (process.argv.includes("--seed-data")) {
           await insertSeedData(keystone);
         }
       },
@@ -70,7 +70,7 @@ export default withAuth(
         !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-      User: `id name email role { ${permissionsList.join(' ')} }`,
+      User: `id name email role { ${permissionsList.join(" ")} }`,
     }),
-  })
+  }),
 );
